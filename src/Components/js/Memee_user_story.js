620105f2616c01917f17212d48685097845dc789
MaulikPatel02 <sutariyamaulik.dignizant@gmail.com>
2022-12-16 14:08:30 +0530

    update

--- src/Components/js/Memee_user_story.js
+++ src/Components/js/Memee_user_story.js
@@ -1,22 +1,19 @@
 import React from "react";
 import "../Css/Memee_user_story.css";
 import Stories from "react-insta-stories";
-import story_1 from "../images/story/story_1.jpg";
-import story_2 from "../images/story/story_2.jpg";
-import story_3 from "../images/story/story_3.jpg";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Memee_user_story() {
+  const location = useLocation();
   const Navigate = useNavigate();
   const story = [
     {
       id: 1,
-      url: story_3,
+      url:process.env.REACT_APP_2_BASE_URL + "/" +location.state.storyData.story,
       duration: 2000,
       header: {
-        heading: "Astronut",
-        subheading: "Posted 30m ago",
-        profileImage: "https://picsum.photos/100/100",
+        heading: location.state.storyData.username,
+        profileImage:process.env.REACT_APP_2_BASE_URL + "/" +location.state.storyData.userProfile
       },
     },
   ];
